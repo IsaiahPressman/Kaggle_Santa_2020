9a9bcfe5d4850a7be01d8866dee4379a5dea4435
Isaiah Pressman <isaiahpressman16@gmail.com>
2020-12-25 01:24:40 -0500

    Added EVERY_STEP_EV as a reward_type option

--- vectorized_env.py
+++ vectorized_env.py
@@ -1,8 +1,10 @@
 from functools import wraps
 import torch
 
-EVERY_STEP = 0
-END_OF_GAME = 1
+EVERY_STEP_TRUE = 0
+EVERY_STEP_EV = 1
+END_OF_GAME_TRUE = 2
+#END_OF_GAME_EV = 3
 
 
 # A vectorized and GPU-compatible recreation of the kaggle "MAB" environment
@@ -15,15 +17,15 @@ class KaggleMABEnvTorchVectorized():
         n_envs=1,
         n_players=2,
         opponent=None,
-        reward_type=EVERY_STEP,
+        reward_type=EVERY_STEP_TRUE,
         normalize_reward=True,
         env_device=torch.device('cuda'),
         out_device=torch.device('cuda'),
     ):
         # Assert parameter conditions
         assert 0 <= decay_rate <= 1.
-        assert reward_type in (END_OF_GAME, EVERY_STEP)
-        if reward_type == END_OF_GAME:
+        assert reward_type in (EVERY_STEP_TRUE, EVERY_STEP_EV, END_OF_GAME_TRUE)
+        if reward_type in (END_OF_GAME_TRUE,):
             assert n_players >= 2
         else:
             assert n_players >= 1
@@ -38,7 +40,7 @@ class KaggleMABEnvTorchVectorized():
         if self.opponent is not None:
             assert n_players == 2
         self.reward_type = reward_type
-        if not normalize_reward or self.reward_type == END_OF_GAME:
+        if not normalize_reward or self.reward_type in (END_OF_GAME_TRUE,):
             self.r_norm = 1.
         else:
             self.r_norm = 1. / (torch.sum(self.decay_rate ** torch.arange(self.n_steps, dtype=torch.float32)) * torch.arange(self.n_bandits, dtype=torch.float32).sum() / (self.n_bandits * self.n_players))
@@ -108,14 +110,16 @@ class KaggleMABEnvTorchVectorized():
         ] += pull_rewards.view(-1)
         
         # Return (obs, reward, done) tuple
-        if self.reward_type == END_OF_GAME:
+        if self.reward_type == EVERY_STEP_TRUE:
+            rewards = pull_rewards
+        elif self.reward_type == EVERY_STEP_EV:
+            rewards = selected_thresholds
+        elif self.reward_type == END_OF_GAME_TRUE:
             rewards = torch.zeros_like(actions).float()
             if self.timestep == self.n_steps:
                 rewards_sums = self.player_rewards_sums.sum(dim=2)
                 winners = rewards_sums.argmax(dim=1)
                 rewards[winners] = 1.
-        elif self.reward_type == EVERY_STEP:
-            rewards = pull_rewards
         
         rewards = rewards * self.r_norm
         # State, reward, done, info_dict
