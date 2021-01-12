Fixed environment thresholds to use double precision, fixed pull_vegas_slot_machines, add pull_vegas_slot_machines_improved

vectorized_agents.py
@@ -120,7 +120,7 @@ class PullVegasSlotMachines(VectorizedAgent):
         else:
             my_pulls, opp_pulls, wins = states.chunk(3, dim=-1)
             losses = my_pulls - wins
-            ev = (wins - losses + opp_pulls - (opp_pulls>0)*1.5) / (wins + losses + opp_pulls)
+            ev = ((wins+1) - losses + opp_pulls - (opp_pulls>0)*1.5) / ((wins+1) + losses + opp_pulls)
             actions = ev.squeeze(-1).argmax(dim=-1)
         return actions
 
@@ -157,16 +157,23 @@ class PullVegasSlotMachinesImproved(VectorizedAgent):
             self.opp_pulls += opp_pull
             if len(self.my_actions_list) >= 2:
                 self.opp_continues = torch.where(
-                    opp_pull == self.opp_last_action,
+                    (opp_pull == 1) & (self.opp_last_action == 1),
                     self.opp_continues + 1.,
                     torch.zeros_like(self.opp_continues)
                 )
+        """
+        print(len(self.my_actions_list))
+        print(self.wins)
+        print(((opp_pull == 1) & (self.opp_last_action == 1)).sum(), (opp_pull == 1) & (self.opp_last_action == 1))
+        print(self.losses)
+        print(self.opp_pulls)
+        print(self.opp_continues)
+        print('\n\n')"""
 
-        print(my_pull.shape)
         win = win[..., my_pull[0] == 1]
         next_bandits = self.get_next_bandit()
         if len(self.my_actions_list) == 0:
-            actions = next_bandits
+            actions = torch.randint(self.n_bandits, size=states.shape[:-2], device=states.device)
         elif len(self.my_actions_list) < 3:
             actions = torch.where(
                 win == 1,
@@ -176,9 +183,9 @@ class PullVegasSlotMachinesImproved(VectorizedAgent):
         else:
             actions_if_three_in_a_row = torch.where(
                 (self.my_actions_list[-1] == self.my_actions_list[-2]) & (
-                            self.my_actions_list[-1] == self.my_actions_list[-3]),
+                        self.my_actions_list[-1] == self.my_actions_list[-3]),
                 torch.where(
-                    torch.rand(next_bandits.shape) < 0.5,
+                    torch.rand(size=next_bandits.shape, device=states.device) < 0.5,
                     self.my_actions_list[-1],
                     next_bandits
                 ),
@@ -192,11 +199,13 @@ class PullVegasSlotMachinesImproved(VectorizedAgent):
 
         self.my_actions_list.append(actions)
         self.opp_last_action = opp_pull.clone()
-        print(next_bandits.shape, win.shape, actions.shape)
+        if len(self.my_actions_list) > 5:
+            #assert False
+            pass
         return actions
 
     def get_next_bandit(self):
-        ev = (self.wins - self.losses + self.opp_pulls - (self.opp_pulls>0)*1.5 + self.opp_continues) \
+        ev = (self.wins - self.losses + self.opp_pulls - (self.opp_pulls > 0)*1.5 + self.opp_continues) \
             / (self.wins + self.losses + self.opp_pulls) \
             * torch.pow(0.97, self.wins + self.losses + self.opp_pulls)
         return ev.argmax(dim=-1)

vectorized_env.py
@@ -123,7 +123,7 @@ class KaggleMABEnvTorchVectorized:
         self.orig_thresholds = torch.randint(
             self.sample_resolution + 1,
             size=(self.n_envs, self.n_bandits),
-            dtype=torch.float32,
+            dtype=torch.float64,
             device=self.env_device
         )
         self.player_n_pulls = torch.zeros((self.n_envs, self.n_players, self.n_bandits), device=self.env_device)
@@ -162,7 +162,7 @@ class KaggleMABEnvTorchVectorized:
         pull_rewards = torch.randint(
             self.sample_resolution,
             size=selected_thresholds.shape,
-            dtype=torch.float32,
+            dtype=selected_thresholds.dtype,
             device=self.env_device) < selected_thresholds
         
         # Update player_n_pulls and player_rewards_sums
@@ -295,7 +295,7 @@ class KaggleMABEnvTorchVectorized:
 
     @property
     def thresholds(self):
-        return self.orig_thresholds * (self.decay_rate ** self.player_n_pulls.sum(dim=1))
+        return self.orig_thresholds * (self.decay_rate ** self.player_n_pulls.sum(dim=1).double())
     
     @property
     def done(self):