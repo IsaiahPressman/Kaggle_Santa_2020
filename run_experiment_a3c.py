08e1914526d8721862c2af87d9c72bbeaee4c2b0
Isaiah Pressman <isaiahpressman16@gmail.com>
2021-01-13 16:42:30 -0500

    latest a3c experiment params

--- run_experiment_a3c.py
+++ run_experiment_a3c.py
@@ -1,6 +1,7 @@
 import base64
 from pathlib import Path
 import pickle
+import shutil
 import torch
 from torch import nn
 
@@ -13,42 +14,36 @@ import vectorized_agents as va
 DEVICE = torch.device('cuda')
 OBS_NORM = 100. / 1999.
 
-use_rnn = False
-def custom_layer_class_factory(*args, **kwargs):
-    def recurrent_layer_class_factory(*args, **kwargs):
-        return nn.LSTM(*args, num_layers=2, **kwargs)
-    return gnn.SmallRecurrentGNNLayer(*args, recurrent_layer_class=recurrent_layer_class_factory, **kwargs)
-
 graph_nn_kwargs = dict(
-    in_features=3,
+    in_features=3 * 1999,
     n_nodes=100,
-    n_hidden_layers=16,
-    layer_sizes=32,
-    layer_class=gnn.SmallRecurrentGNNLayer if use_rnn else gnn.SmallFullyConnectedGNNLayer,
-    #layer_class=custom_layer_class_factory,
+    n_hidden_layers=3,
+    layer_sizes=[256, 256, 64, 64],
+    layer_class=gnn.FullyConnectedGNNLayer,
+    normalize=True,
     skip_connection_n=1
 )
 model = gnn.GraphNNActorCritic(**graph_nn_kwargs)
 
-with open(f'runs/small_16_32_v1/570_cp.txt', 'r') as f:
+with open(f'runs/a3c/_starting_cp.txt', 'r') as f:
     serialized_string = f.readline()[2:-1].encode()
 state_dict_bytes = base64.b64decode(serialized_string)
 loaded_state_dicts = pickle.loads(state_dict_bytes)
 model.load_state_dict(loaded_state_dicts['model_state_dict'])
 
 model.to(device=DEVICE)
-optimizer = torch.optim.Adam(model.parameters())
+optimizer = torch.optim.Adam(model.parameters(),
+                             weight_decay=5e-6)
 
 env_kwargs = dict(
     env_device=DEVICE,
     out_device=DEVICE,
     normalize_reward=False,
     reward_type=ve.EVERY_STEP_EV_ZEROSUM,
-    obs_type=ve.LAST_STEP_OBS if use_rnn else ve.SUMMED_OBS,
-    opponent_obs_type=ve.SUMMED_OBS
+    obs_type=ve.EVERY_STEP_OBS_RAVELLED
 )
 rl_alg_kwargs = dict(
-    batch_size=30 if use_rnn else 30,
+    batch_size=30,
     gamma=0.99
 )
 
@@ -60,30 +55,37 @@ rl_agent_opp_kwargs = dict(
     deterministic_policy=True
 )
 initial_opponent_pool = [
-    #va.BasicThompsonSampling(OBS_NORM),
-    va.PullVegasSlotMachines(OBS_NORM),
+    #va.RandomAgent(),
+    va.BasicThompsonSampling(OBS_NORM),
+    va.PullVegasSlotMachinesImproved(OBS_NORM),
     #va.SavedRLAgent('a3c_agent_v0', device=DEVICE),
     va.SavedRLAgent('a3c_agent_v1', **rl_agent_opp_kwargs),
     va.SavedRLAgent('a3c_agent_v2', **rl_agent_opp_kwargs),
-    va.SavedRLAgent('a3c_agent_v3', **rl_agent_opp_kwargs),
-    va.SavedRLAgent('a3c_agent_v4-162', **rl_agent_opp_kwargs),
+    #va.SavedRLAgent('a3c_agent_v3', **rl_agent_opp_kwargs),
+    #va.SavedRLAgent('a3c_agent_v4-162', **rl_agent_opp_kwargs),
     va.SavedRLAgent('a3c_agent_small_8_32-790', **rl_agent_opp_kwargs),
 ]
 
-folder_name = f"small_{graph_nn_kwargs['n_hidden_layers']}_{graph_nn_kwargs['layer_sizes']}_v2"
-a3c_alg = A3CVectorized(model_constructor, optimizer, model=model, device=DEVICE,
+#folder_name = f"small_{graph_nn_kwargs['n_hidden_layers']}_{graph_nn_kwargs['layer_sizes']}_v2"
+folder_name = 'TEMP'
+a3c_alg = A3CVectorized(model_constructor, optimizer, env_kwargs['obs_type'],
+                        model=model, device=DEVICE,
                         exp_folder=Path(f'runs/a3c/{folder_name}'),
-                        recurrent_model=use_rnn,
                         clip_grads=10.,
                         play_against_past_selves=True,
                         n_past_selves=4,
-                        checkpoint_freq=10,
+                        checkpoint_freq=20,
+                        log_params_full=False,
                         initial_opponent_pool=initial_opponent_pool,
-                        opp_posterior_decay=0.95)
+                        opp_posterior_decay=0.99)
+this_script = Path(__file__).absolute()
+shutil.copy(this_script, a3c_alg.exp_folder / f'_{this_script.name}')
 
-env_kwargs['n_envs'] = 200
+env_kwargs['n_envs'] = 32
+#env_kwargs['opponent'] = va.BasicThompsonSampling(OBS_NORM)
+#env_kwargs['opponent_obs_type'] = ve.SUMMED_OBS
 try:
-    a3c_alg.train(n_episodes=1000, **rl_alg_kwargs, **env_kwargs)
+    a3c_alg.train(n_episodes=10000, **rl_alg_kwargs, **env_kwargs)
 except KeyboardInterrupt:
     if a3c_alg.true_ep_num > a3c_alg.checkpoint_freq:
         print('KeyboardInterrupt: saving model')
